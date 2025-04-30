b9882686d955ef1b51bb668fd638bbdf685a1ed2
pradeep-145 <spradeepsakthi2004@gmail.com>
2025-04-30 15:22:37 +0530

    merge

--- backend/middlewares/customer.middleware.js
+++ backend/middlewares/customer.middleware.js
@@ -15,18 +15,11 @@ const authenticateJWT = async (req, res, next) => {
     const jwtCookie = cookies.split('; ').find(cookie => cookie.startsWith('jwt='));
     if (!jwtCookie) {
       throw new Error('JWT token not found in cookies');
-    const authHeader = req.headers["cookie"];
-    console.log(req.headers)
-    if (!authHeader || !authHeader.startsWith("jwt")) {
-      throw new Error("Authorization header missing or malformed");
     }
 
     const token = jwtCookie.split('=')[1];
-    
-    const token = authHeader.split("=")[1];
     if (!token) {
       throw new Error('Token is empty');
-      throw new Error("Token not found");
     }
     
     const decoded = await JwtService.verifyToken(token);
@@ -35,7 +28,6 @@ const authenticateJWT = async (req, res, next) => {
     }
     
     const user = await customerModel.findById(decoded.userId);
-    console.log(user);
     console.log(user);
     if (!user) {
       console.log(req.headers);
