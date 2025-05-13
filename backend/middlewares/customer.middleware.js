196f0fd8d669ef8bab194a096001fa7d8c0adc63
pradeep-145 <spradeepsakthi2004@gmail.com>
2025-05-13 08:56:39 +0530

    fix: Clean up authorization header checks in JWT middleware

--- backend/middlewares/customer.middleware.js
+++ backend/middlewares/customer.middleware.js
@@ -3,12 +3,13 @@ const customerModel = require("../models/customer.model.js");
 
 const authenticateJWT = async (req, res, next) => {
   try {
+
     if (!req.headers["authorization"]) {
       throw new Error("Authorization header is missing");
     }
 
     const token = req.headers["authorization"].split(" ")[1];
-
+    console.log(req.headers)
     if (!token) {
       throw new Error("Token is empty");
     }
