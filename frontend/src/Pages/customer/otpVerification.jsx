a1473ac703327fe71323049aa665051f03c83817
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2025-05-21 18:41:19 +0530

    feat: Add "Back to Login" button in OTP verification for improved navigation

--- frontend/src/Pages/customer/otpVerification.jsx
+++ frontend/src/Pages/customer/otpVerification.jsx
@@ -328,6 +328,7 @@ const OtpVerification = () => {
       </div>
       
       {!isFromLogin && (
+        <div>
         <div className="mt-3 text-center">
           <button 
             type="button" 
@@ -337,6 +338,17 @@ const OtpVerification = () => {
             Change Email
           </button>
         </div>
+        <div className="text-center">
+          <button 
+            type="button" 
+            onClick={() => navigate('/customer/login')}
+            className="btn btn-link text-yellow-600 hover:text-yellow-700"
+          >
+            Back to Login
+          </button>
+        </div>
+        </div>
+        
       )}
     </>
   );
@@ -394,6 +406,17 @@ const OtpVerification = () => {
             'Reset Password'
           }
         </button>
+
+        <div className="text-center">
+          <button 
+            type="button" 
+            onClick={() => navigate('/customer/login')}
+            className="btn btn-link text-yellow-600 hover:text-yellow-700"
+          >
+            Back to Login
+          </button>
+        </div>
+        
       </form>
     </>
   );
